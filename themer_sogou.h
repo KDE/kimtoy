bd9dcbf7c2f6ed09ddc3db254c524b31c3a07802
Ni Hui <shuizhuyuanluo@126.com>
2011-09-28 20:38:52 +0800

    correct h_XXX

--- themer_sogou.h
+++ themer_sogou.h
@@ -5,13 +5,13 @@
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU General Public License as
  *  published by the Free Software Foundation; either version 2 of
- *  the License or (at your oh_ption) version 3 or any later version
- *  acceh_pted by the membership of KDE e.V. (or its successor approved
- *  by the membership of KDE e.V.), which shall act as a h_proxy
+ *  the License or (at your option) version 3 or any later version
+ *  accepted by the membership of KDE e.V. (or its successor approved
+ *  by the membership of KDE e.V.), which shall act as a proxy
  *  defined in Section 14 of version 3 of the license.
  *
  *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the imh_plied warranty of
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
  *
