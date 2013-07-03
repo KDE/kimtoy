1f0aca70e13de2a86ab1d51c6d7340a9e7ecb8f5
Ni Hui <shuizhuyuanluo@126.com>
2013-07-03 23:13:56 +0800

    make ctrl+space work for ibus 1.5

--- ibus-panel/main.c
+++ ibus-panel/main.c
@@ -27,9 +27,82 @@
 static IBusBus *bus = NULL;
 static IBusPanelImpanel *impanel = NULL;
 
+#include <X11/Xlib.h>
+#include <X11/Xutil.h>
+
+typedef struct _x11_source {
+    GSource source;
+    Display *dpy;
+} x11_source_t;
+
+static gboolean
+x11_fd_prepare(GSource *source, gint *timeout)
+{
+    *timeout = -1;
+    return FALSE;
+}
+
+static gboolean
+x11_fd_check (GSource *source)
+{
+    return TRUE;
+}
+
+static gboolean
+x11_fd_dispatch(GSource* source, GSourceFunc callback, gpointer user_data)
+{
+    Display *dpy = ((x11_source_t*)source)->dpy;
+
+    XEvent e;
+
+    while (XPending(dpy))
+    {
+
+        XNextEvent(dpy, &e);
+        if (e.type == KeyPress)
+        {
+//             g_print("KeyPress!!!\n");
+
+            static const gchar *names[64] = {0};
+            names[0] = "xkb:us::eng";
+
+            static int inited = 0;
+            if (!inited)
+            {
+                IBusConfig *config = ibus_bus_get_config (bus);
+                GVariant *engines = ibus_config_get_value (config, "general", "preload_engines");
+
+                int i = 1;
+
+                GVariantIter iter;
+                GVariant *child;
+                g_variant_iter_init (&iter, engines);
+                while ((child = g_variant_iter_next_value (&iter)) != NULL) {
+                    const gchar *engine_name = g_variant_get_string (child, NULL);
+//                     g_print("engine_name %s\n", engine_name);
+                    names[i] = engine_name;
+                    i++;
+                    g_variant_unref (child);
+                }
+
+                inited = 1;
+            }
+
+            static int n = 0;
+            n = n==0? 1 : 0;
+            ibus_bus_set_global_engine(bus, names[n]);
+        }
+
+    }
+
+    return TRUE;
+}
+
 /* options */
 static gboolean ibus = FALSE;
 static gboolean verbose = FALSE;
+
+static GMainLoop *main_loop = NULL;
 
 static const GOptionEntry entries[] =
 {
@@ -42,7 +115,13 @@ static void
 ibus_disconnected_cb (IBusBus  *bus,
                       gpointer  user_data)
 {
+#if !IBUS_CHECK_VERSION(1,4,99)
     ibus_quit ();
+#else
+    if (main_loop) {
+        g_main_loop_quit (main_loop);
+    }
+#endif
 }
 
 static void
@@ -57,7 +136,59 @@ ibus_impanel_start (void)
     impanel = ibus_panel_impanel_new (ibus_bus_get_connection (bus));
     ibus_bus_request_name (bus, IBUS_SERVICE_PANEL, 0);
     ibus_panel_impanel_set_bus(impanel, bus);
+#if !IBUS_CHECK_VERSION(1,4,99)
     ibus_main ();
+#else
+
+    Display*    dpy     = XOpenDisplay(0);
+    Window      root    = DefaultRootWindow(dpy);
+    XEvent      ev;
+
+    unsigned int    modifiers       = ControlMask;
+    int             keycode         = XKeysymToKeycode(dpy, XK_space);
+
+    XGrabKey(dpy, keycode, modifiers, root, False, GrabModeAsync, GrabModeAsync);
+    XGrabKey(dpy, keycode, modifiers | LockMask, root, False, GrabModeAsync, GrabModeAsync); // capslock
+    XGrabKey(dpy, keycode, modifiers | Mod2Mask, root, False, GrabModeAsync, GrabModeAsync); // numlock
+    XGrabKey(dpy, keycode, modifiers | LockMask | Mod2Mask, root, False, GrabModeAsync, GrabModeAsync);
+
+    XSelectInput(dpy, root, KeyPressMask);
+
+//     XCloseDisplay(dpy);
+
+    main_loop = g_main_loop_new(NULL, FALSE);
+
+    int dpyfd = ConnectionNumber(dpy);
+    GPollFD dpy_pollfd = {dpyfd, G_IO_IN | G_IO_HUP | G_IO_ERR, 0};
+
+    GSourceFuncs x11_source_funcs = {
+        x11_fd_prepare,
+        x11_fd_check,
+        x11_fd_dispatch,
+        NULL, /* finalize */
+        NULL, /* closure_callback */
+        NULL /* closure_marshal */
+    };
+
+    GSource *x11_source = g_source_new(&x11_source_funcs, sizeof(x11_source_t));
+    ((x11_source_t*)x11_source)->dpy = dpy;
+
+    g_source_add_poll(x11_source, &dpy_pollfd);
+    g_source_set_can_recurse(x11_source, TRUE);
+    g_source_attach(x11_source, NULL);
+
+    g_main_loop_run (main_loop);
+
+    g_main_loop_unref (main_loop);
+    main_loop = NULL;
+
+    XUngrabKey(dpy, keycode, modifiers, root);
+    XUngrabKey(dpy, keycode, modifiers | LockMask, root);
+    XUngrabKey(dpy, keycode, modifiers | Mod2Mask, root);
+    XUngrabKey(dpy, keycode, modifiers | LockMask | Mod2Mask, root);
+
+    XCloseDisplay(dpy);
+#endif
 }
 
 gint
